740208ffeaceea9490396b776bce4f51201442d3
Isaac Tait <isaac@mountaintopcoding.com>
2020-06-24 14:32:55 -0700

    I feel like I am getting closer...

--- app.js
+++ app.js
@@ -10,32 +10,36 @@ function compileUniqueID() {
     };
     console.log(getYear());
 
+    const btn = document.querySelector('#btn');
     // Takes the region selected from the radio button menu in index.html and assigns a unique two digit number to the specific region.
-    let setRegion = parseInt(document.getElementsByName('region').value)
-    //setRegion = "North America"
     
-    let region = () => {
-        if(setRegion === "Africa") {
-            return "01"
-        } else if(setRegion === "Asia") {
-            return "02"
-        } else if(setRegion === "Australia") {
-            return "03"
-        } else if(setRegion === "Europe") {
-            return "04" 
-        } else if(setRegion === "North America") {
-            return "05"
-        } else if(setRegion === "South America") {
-            return "06"
-        } else if(setRegion === "Russia") {
-            return "07"
-        } else if(setRegion === "Japan") {
-            return "08"
-        } else if (setRegion === "North and South Poles") {
-            return "09"
-        } else return "Select a region please" 
+    btn.onclick = region = () => {
+        let setRegions = document.querySelectorAll('input[name="region"]')
+        
+        for (const setRegion of setRegions) {
+            if(setRegion.checked === "Africa") {
+                return "01"
+            } else if(setRegion.checked === "Asia") {
+                return "02"
+            } else if(setRegion.checked === "Australia") {
+                return "03"
+            } else if(setRegion.checked === "Europe") {
+                return "04" 
+            } else if(setRegion.checked === "North America") {
+                return "05"
+            } else if(setRegion.checked === "South America") {
+                return "06"
+            } else if(setRegion.checked === "Russia") {
+                return "07"
+            } else if(setRegion.checked === "Japan") {
+                return "08"
+            } else if (setRegion.checked === "North and South Poles") {
+                return "09"
+            } else return "Select a region please" 
+        }
+        return region();
     };
-    console.log(region());
+    console.log(region);
 
     // Compiles the year and the region and the users randomly generated five digit id together into a string and appends the text "LTM" (Life Time Member) to the front of the unique ID number
     return "LTM" + getYear() + region() + Math.floor(10000 + Math.random() * 90000)
